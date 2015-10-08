b2c9107edfc6a6960ff5db90eb12146988fb7882
Elegia <lauri.m.lukka@gmail.com>
2015-10-08 10:11:13 +0300

    Made a new repo

---
+++ Assets/Scripts/FPS_translate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/FPS translate")]
+public class FPS_translate : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+        Quaternion cameraRotation = this.transform.rotation;
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+        float tmp = Input.GetAxis("Vertical") * speed;
+        movement.z = tmp;
+        tmp = Input.GetAxis("Horizontal") * speed;
+        movement.x = tmp;
+
+        movement = cameraRotation * movement;
+        movement.y = 0.0f;
+
+        this.transform.position += movement * Time.deltaTime;
+
+    }
+
+  
+}
---
+++ Assets/SoundOnOff.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundOnOff : MonoBehaviour {
+
+    public bool inEffectZone = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        GameObject userInterface = GameObject.Find("Instruction");
+        userInterface.GetComponent<Text>().text = "Press left mouse button.";
+
+        inEffectZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        GameObject userInterface = GameObject.Find("Instruction");
+        userInterface.GetComponent<Text>().text = "Go back to Mona Lisa. It was not nice leaving her.";
+
+        inEffectZone = false;
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Stop();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inEffectZone)
+        {
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+
+                GameObject userInterface = GameObject.Find("Instruction");
+                userInterface.GetComponent<Text>().text = "LOL";
+
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.Play();
+            }
+
+        }
+	}
+}
