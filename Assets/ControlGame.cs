6ece126ffe970d6487e93edd7e848d20f71eb213
JooseRajamaeki <joose.rajamaki@gmail.com>
2015-10-19 14:59:37 +0300

    Added UI-stuff
    Added UI components

---
+++ Assets/ControlGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject startUI = GameObject.Find("StartUI");
+
+        GameObject inactivateThis = GameObject.Find("GamePlay");
+        inactivateThis.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour
+{
+
+    void Start()
+    {
+        Application.Quit();
+    }
+
+}
+
+
