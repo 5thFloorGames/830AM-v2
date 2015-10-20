a98860409fef2aced111c87ec44b818bc49988a7
JooseRajamaeki <joose.rajamaki@gmail.com>
2015-10-20 10:38:59 +0300

    Some scripts for showing choises.
    Scripts for showing choises.

---
+++ Assets/HoverText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverText : MonoBehaviour {
+
+    public GameObject objectToFollow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+        if (objectToFollow != null)
+        {
+            Transform transform = GetComponent<Transform>();
+            Transform objectTransform = objectToFollow.GetComponent<Transform>();
+
+            transform.position = objectTransform.position;
+
+        }
+
+	}
+}
---
+++ Assets/ShowChoises.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowChoises : MonoBehaviour {
+
+    TextMesh[] choises = null;
+    public float disappearTime = 3.0f;
+    float timeCount = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        choises = gameObject.GetComponentsInChildren<TextMesh>();
+
+        foreach(TextMesh choise in choises){
+            choise.gameObject.SetActive(false);
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        ExplorationReaction exploration = GetComponent<ExplorationReaction>();
+
+        bool someChoisePicked = false;
+
+        foreach(TextMesh choise in choises)
+        {
+            if (choise.gameObject.GetComponent<ExplorationReaction>().explored)
+            {
+                someChoisePicked = true;
+            }
+        }
+        
+
+        if (exploration.explored && !someChoisePicked)
+        {
+
+            foreach (TextMesh choise in choises)
+            {
+                choise.gameObject.SetActive(true);
+            }
+        }
+
+        if (exploration.explored)
+        {
+            timeCount += Time.deltaTime;
+
+            if (timeCount > disappearTime)
+            {
+                exploration.explored = false;
+                timeCount = 0.0f;
+
+                foreach (TextMesh choise in choises)
+                {
+                    choise.gameObject.SetActive(false);
+                }
+            }
+        }
+
+    }
+}
