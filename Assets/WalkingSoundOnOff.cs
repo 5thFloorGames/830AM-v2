e0d79a540d41966969360d3ddec8f98a573d3ac9
jacksonf <Jackson@Sakus-MacBook-Pro.local>
2015-10-20 15:02:59 +0300

    some sounds added

---
+++ Assets/WalkingSoundOnOff.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingSoundOnOff : MonoBehaviour {
+
+	bool isWalking = false;
+	bool hasBeenWalking = false;
+	public float stopSpeed = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+		Rigidbody body = GetComponent<Rigidbody> ();
+
+		if (body.velocity.magnitude > stopSpeed) {
+			isWalking = true;
+		} else {
+
+			isWalking = false;
+			hasBeenWalking = false;
+
+			GameObject footSteps = GameObject.Find("FootstepSounds");
+			
+			if (footSteps){
+				AudioSource audio = footSteps.GetComponent<AudioSource>();
+				audio.Stop();
+			}
+		}
+
+		if (isWalking) {
+
+			if (!hasBeenWalking) {
+
+				GameObject footSteps = GameObject.Find("FootstepSounds");
+
+				if (footSteps){
+					AudioSource audio = footSteps.GetComponent<AudioSource>();
+					audio.Play();
+				}
+
+				hasBeenWalking = true;
+			}
+		}
+
+	}
+}
